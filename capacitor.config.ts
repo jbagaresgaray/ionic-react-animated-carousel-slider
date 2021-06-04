6175b41651087f26ccf5fbf14b7084ef51e44627
Philip Cesar Garay <philipgaray2@gmail.com>
2021-06-04 19:04:54 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-animated-carousel-slider',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
